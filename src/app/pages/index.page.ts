926fc6696b1a79aec24c22b74787389ad7012cc9
charles-fulcrum <charles@nyfulcrum.com>
2024-03-02 14:09:01 +0800

    Update home page

--- src/app/pages/index.page.ts
+++ src/app/pages/index.page.ts
@@ -1,18 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { MainLayoutComponent } from '@app/layouts/main-layout/main-layout.component';
 import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink, MainLayoutComponent, ButtonModule],
+  imports: [RouterLink, InputTextModule, ButtonModule],
   templateUrl: './index.page.html',
 })
-export default class HomePageComponent {
-  count = 0;
-
-  increment() {
-    this.count++;
-  }
-}
+export default class HomePageComponent {}
